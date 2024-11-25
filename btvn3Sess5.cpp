f89767be042cecb883aa61161aac7f89686d9bdc
Nguyen-Quang-Huy-08-05-2006 <thaohyduonghuy@gmail.com>
2024-11-26 01:12:04 +0700

    Update btvn3Sess5.cpp

--- btvn3Sess5.cpp
+++ btvn3Sess5.cpp
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main(){
+	int b;
 	int d;
 	int a;
 	printf("nhap vao 1 so nguyen: ");
 	scanf("%d",&a);
-	for(int b=1; b>=a; b++){
+	for(b>=a; b++){
 		for(int c=0; c>=a; c++){
-			int d=b+c;
+			printf("%d=%d+%d", d=a+b);
 		}
 	}
-	printf("%d", &d);
+	printf("%d", d);
 	
 	return 0;
 	
